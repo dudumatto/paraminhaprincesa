8aa883b107438ebfa1b425852546fd986d4b65e4
dumatto <mattosdudu2009@gmail.com>
2026-05-21 13:12:36 -0300

    ajuste texto

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const DATA_INICIO_NAMORO = "2026-01-24T15:30:00-03:00";
 const TEMPO_LOADING_MS = 7600;
 
 const FRASES_ROMANTICAS = {
-  intro: "Desde o primeiro dia, cada detalhe ficou mais bonito porque existe você.",
+  intro: "Desde o primeiro dia, cada detalhe ficou mais bonito porque existe você!!",
   contador: "E a nossa história continua florescendo.",
   mensagem:
     `Meu amor,
